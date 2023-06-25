c482e4846d9b8c441f0110a0fdb0b4dc0889f419
Charles Chua <chuacharlesbon@gmail.com>
2023-06-25 20:38:50 +0800

    Updated: Meta Tags

--- web/js/change_meta.js
+++ web/js/change_meta.js
@@ -1,5 +1,6 @@
 function changeMeta() {
     let $metaOgTitle = document.querySelector('meta[property="og:title"]')
+    let $metaDescription = document.querySelector('meta[name="description"]')
     // If a <link rel="icon"> element already exists,
     // change its href to the given link.
     if ($metaOgTitle !== null) {
@@ -10,5 +11,15 @@ function changeMeta() {
         $metaOgTitle.property = "og:title"
         $metaOgTitle.content = 'Full Stack Developer - Profile'
         document.head.appendChild($metaOgTitle)
+    }
+
+    if ($metaDescription !== null) {
+        $metaDescription.content = 'Full Stack Developer - Profile'
+        // Otherwise, create a new element and append it to <head>.
+    } else {
+        $metaDescription = document.createElement("meta")
+        $metaDescription.name = "description"
+        $metaDescription.content = 'Developer Portfolio - Charles Bon Chua: Full Stack Developer; Frontend React and Flutter, Backend Nodejs'
+        document.head.appendChild($metaDescription)
     }
 }
