08be4ff23f4875cf6b1d5f9d77aac7eeb7d44567
Charles Chua <chuacharlesbon@gmail.com>
2023-06-26 14:34:00 +0800

    Updated: Main Screen - SEO Tags

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:meta_seo/meta_seo.dart';
 import 'package:myflutterprofile/components/app_title_builder.dart';
 import 'package:myflutterprofile/presentations/home/home_screen.dart';
 import 'package:myflutterprofile/presentations/splash/splash_screen.dart';
@@ -10,6 +11,11 @@ import 'package:url_strategy/url_strategy.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Meta SEO
+  if (kIsWeb) {
+    MetaSEO().config();
+  }
 
   // Here we set the URL strategy for our web app.
   // This will remove /#/
@@ -29,6 +35,18 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+
+    if (kIsWeb) {
+      // Define MetaSEO object
+      MetaSEO meta = MetaSEO();
+      // Add meta seo data for web app as you want
+      meta.author(author: 'Charles Bon Chua');
+      meta.keywords(keywords: 'Flutter, Dart, React, Developer, Software engineer');
+      meta.ogTitle(ogTitle: 'Developer Portfolio');
+      meta.ogDescription(ogDescription: 'Developer Portfolio - Charles Bon Chua: Full Stack Developer; Frontend React and Flutter, Backend Nodejs.');
+      meta.ogImage(ogImage: 'https://github.com/chuacharlesbon/portfolio-chua/blob/master/images/og-image.png?raw=true');
+    }
+
     return MaterialApp.router(
       scrollBehavior: MyCustomScrollBehavior(),
       title: 'My Profile',
