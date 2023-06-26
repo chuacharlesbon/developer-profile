5a19276be6cc5e25675df4136fd18c8e434d0cc6
Charles Chua <chuacharlesbon@gmail.com>
2023-06-26 11:45:07 +0800

    Updated: Home Screen - SEO Tags

--- lib/presentations/home/home_screen.dart
+++ lib/presentations/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:myflutterprofile/core/utils/change_favicon.dart';
 import 'package:myflutterprofile/resources/constants/urls.dart';
+import 'package:seo/seo.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -33,29 +34,36 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     log('Current Route: ${GoRouter.of(context).location}');
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'My Profile',
-            ),
-            Text(
-              'Hello',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return Seo.head(
+      tags: const [
+        MetaTag(
+          name: "description",
+          content: "Developer Portfolio - Charles Bon Chua: Full Stack Developer; Frontend React and Flutter, Backend Nodejs."),
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'My Profile',
+              ),
+              Text(
+                'Hello',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
